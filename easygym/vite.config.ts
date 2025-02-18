bf06b0899eec8ccad9b649bd52a814538ecf74c5
Diogo Carpinelli <diogocarpinelli78@gmail.com>
2025-02-18 12:31:17 -0300

    v0.0.3 - alterações de conf para teste deploy
    Signed-off-by: Diogo Carpinelli <diogocarpinelli78@gmail.com>

--- easygym/vite.config.ts
+++ easygym/vite.config.ts
@@ -4,14 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
+      include: /.(jsx|tsx)$/,
       babel: {
-        plugins: [
-          "babel-plugin-styled-components"
-        ]
-      }
-    })
+        plugins: ['styled-components'],
+        babelrc: false,
+        configFile: false,
+      },
+    }),
   ],
-  define: {
-    "process.env": {}
-  }
 });
