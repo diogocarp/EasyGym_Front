54f584ba79a1034d1a896e003b41a782b54d553c
Diogo Carpinelli <diogocarpinelli78@gmail.com>
2025-03-05 21:14:31 -0300

    v0.0.8 - inserção das telas de planos na sidebar de membros, e criação das telas de membros e planos no operador
    Signed-off-by: Diogo Carpinelli <diogocarpinelli78@gmail.com>

--- easygym/src/main.tsx
+++ easygym/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 'use client';
 
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
