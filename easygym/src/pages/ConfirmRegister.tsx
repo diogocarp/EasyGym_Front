f551f75cb064016c0fb965b87487e0c7ec7d9603
Diogo Carpinelli <diogocarpinelli78@gmail.com>
2025-06-24 17:28:29 -0300

    v0.0.29 - ajuste para deploy
    Signed-off-by: Diogo Carpinelli <diogocarpinelli78@gmail.com>

--- easygym/src/pages/ConfirmRegister.tsx
+++ easygym/src/pages/ConfirmRegister.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
